a0596fbd21631e36b4f170194ca3a21db8417626
Rob Moran <rob.moran@arm.com>
2020-08-23 16:02:19 +0100

    Skip signing on Linux

--- scripts/after-build.js
+++ scripts/after-build.js
@@ -3,7 +3,13 @@ const child_process = require('child_process');
 
 exports.default = async function (buildResult) {
 
-    const appPath = buildResult.artifactPaths.find((artifactPath) => path.extname(artifactPath) === '.dmg' || path.extname(artifactPath) === '.exe');
+    const appPath = buildResult.artifactPaths.find(artifactPath => path.extname(artifactPath) === '.dmg' || path.extname(artifactPath) === '.exe');
+
+    if (!appPath) {
+        console.log('Unable to find macOS or Windows installer to sign', buildResult.artifactPaths);
+        return;
+    }
+
     const entitlements = path.resolve('..', 'entitlements.plist');
     const output = path.join(path.dirname(appPath), `signed-${path.basename(appPath)}`);
     const command = path.join(__dirname, 'sign.sh');
