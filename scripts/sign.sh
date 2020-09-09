227a4e9e970c855880a49751b652aacc1c5c520c
thegecko <rob.moran@arm.com>
2020-09-09 12:17:13 +0100

    du

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -x
 
-set -x
-
 INPUT=$1
 ENTITLEMENTS=$2
 
@@ -19,13 +17,15 @@ scp $ENTITLEMENTS genie.theia@projects-storage.eclipse.org:./entitlements.plist
 # sign over ssh
 ssh -q genie.theia@projects-storage.eclipse.org curl -o signed.zip -F file=@unsigned.zip -F entitlements=@entitlements.plist http://build.eclipse.org:31338/macsign.php
 ssh -q genie.theia@projects-storage.eclipse.org cat signed.zip
-ssh -q genie.theia@projects-storage.eclipse.org stat -c%s signed.zip
+ssh -q genie.theia@projects-storage.eclipse.org du -h signed.zip
+ssh -q genie.theia@projects-storage.eclipse.org stat signed.zip
 
 # copy signed app back from server
 scp genie.theia@projects-storage.eclipse.org:./signed.zip ./signed.zip
 
 cat signed.zip
-stat -c%s signed.zip
+du -h signed.zip
+stat signed.zip
 
 # unzip app
 unzip -qq signed.zip
