6f5f990831f59f5eec02bf1d9ad2ed1db8634784
thegecko <rob.moran@arm.com>
2020-09-17 10:27:39 +0100

    Remove testdata zips

--- scripts/after-pack.js
+++ scripts/after-pack.js
@@ -1,7 +1,23 @@
 const path = require('path');
+const util = require('util');
 const child_process = require('child_process');
+const rimraf = require('rimraf');
+
+const asynfRimraf = util.promisify(rimraf);
+
+const DELETE_PATHS = [
+    'Contents/Resources/app/node_modules/unzip-stream/testData*'
+];
 
 exports.default = async function (context) {
+
+    const appName = `${context.packager.appInfo.productFilename}.app`;
+    const appPath = path.resolve(context.appOutDir, appName);
+
+    // Remove anything we don't want in the final package
+    for (const deletePath of DELETE_PATHS) {
+        await asynfRimraf(path.resolve(appPath, deletePath));
+    }
 
     // Only continue for macOS
     if (context.packager.platform.name !== 'mac') {
@@ -9,11 +25,10 @@ exports.default = async function (context) {
     }
 
     const command = path.join(__dirname, 'sign.sh');
-    const appPath = `${context.packager.appInfo.productFilename}.app`;
     const entitlements = path.resolve(__dirname, '..', 'entitlements.plist');
 
     child_process.execFileSync(command, [
-        appPath,
+        appName,
         entitlements
     ], {
         cwd: context.appOutDir
