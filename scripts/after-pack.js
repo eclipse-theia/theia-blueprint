aaa04798b8dbbfb45564a5000c346c1d5d72bb0f
thegecko <rob.moran@arm.com>
2020-09-18 18:51:07 +0100

    Sign binaries individually

--- scripts/after-pack.js
+++ scripts/after-pack.js
@@ -2,8 +2,16 @@ const path = require('path');
 const util = require('util');
 const child_process = require('child_process');
 const rimraf = require('rimraf');
+const glob = require('glob');
+const fse = require('fs-extra');
 
 const asynfRimraf = util.promisify(rimraf);
+
+const BINARY_EXTS = [
+    '',
+    '.node',
+    '.dylib'
+];
 
 const DELETE_PATHS = [
     'Contents/Resources/app/node_modules/unzip-stream/testData*'
@@ -27,6 +35,27 @@ exports.default = async function (context) {
     const command = path.join(__dirname, 'sign.sh');
     const entitlements = path.resolve(__dirname, '..', 'entitlements.plist');
 
+    // Find all executable files and sign them
+    const files = glob.sync('**/*', { cwd: appPath, realpath: true });
+    files.forEach(file => {
+        const stat = fse.lstatSync(file);
+        const isExecutableFile = stat.isFile() && (stat.mode & fse.constants.S_IXOTH);
+        const pathParsed = path.parse(file);
+
+        if (isExecutableFile && BINARY_EXTS.indexOf(pathParsed.ext) > -1) {
+            console.log(`Signing ${file}...`);
+
+            child_process.execFileSync(command, [
+                file,
+                entitlements
+            ], {
+                cwd: context.appOutDir
+            });
+        }
+    });
+
+    // Sign final app
+    console.log(`Signing ${appName}...`);
     child_process.execFileSync(command, [
         appName,
         entitlements
--- scripts/sign.sh
+++ scripts/sign.sh
@@ -3,36 +3,37 @@
 INPUT=$1
 ENTITLEMENTS=$2
 
-# zip app
-chmod -R a-st $INPUT
-zip -r -q --symlinks unsigned.zip $INPUT
-rm -rf $INPUT
+FILE="$INPUT"
+NEEDS_ZIP=false
 
-# ensure storage server is clean
-ssh -q genie.theia@projects-storage.eclipse.org rm -f unsigned.zip signed.zip entitlements.plist
+# if folder, zip it
+if [ -d "$INPUT" ]; then
+    FILE=unsigned.zip
+    NEEDS_ZIP=true
 
-# copy zip to storage server
-scp unsigned.zip genie.theia@projects-storage.eclipse.org:./unsigned.zip
+    chmod -R a-st $INPUT
+    zip -r -q --symlinks $FILE $INPUT
+    rm -rf $INPUT
+fi
+
+# copy file to storage server
+scp $FILE genie.theia@projects-storage.eclipse.org:./
+rm -f $FILE
 
 # copy entitlements to storage server
 scp $ENTITLEMENTS genie.theia@projects-storage.eclipse.org:./entitlements.plist
 
 # sign over ssh
-ssh -q genie.theia@projects-storage.eclipse.org curl -o signed.zip -F file=@unsigned.zip -F entitlements=@entitlements.plist http://build.eclipse.org:31338/macsign.php
+ssh -q genie.theia@projects-storage.eclipse.org curl -o signed-$FILE -F file=@$FILE -F entitlements=@entitlements.plist http://build.eclipse.org:31338/macsign.php
 
-# copy signed app back from server
-scp genie.theia@projects-storage.eclipse.org:./signed.zip ./signed.zip
+# copy signed file back from server
+scp genie.theia@projects-storage.eclipse.org:./signed-$FILE $FILE
 
-# check for size
-actualSize=$(stat -f%z signed.zip)
+# ensure storage server is clean
+ssh -q genie.theia@projects-storage.eclipse.org rm -f $FILE signed-$FILE entitlements.plist
 
-# file is too small, it's likely an error so show it
-if [ $actualSize -lt 40000000 ]; then
-    echo "signed.zip is just $actualSize bytes large!"
-    output=$(cat signed.zip)
-    echo "$output"
+# if unzip needed
+if [ "$NEEDS_ZIP" = true ]; then
+    unzip -qq $FILE
+    rm -f $FILE
 fi
-
-# unzip app
-unzip -qq signed.zip
-rm -f unsigned.zip signed.zip
