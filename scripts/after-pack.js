3ecab89dbe5cc544e906c3425e3b090666b497c4
thegecko <rob.moran@arm.com>
2020-09-21 15:43:45 +0100

    Sign in-place

--- scripts/after-pack.js
+++ scripts/after-pack.js
@@ -3,7 +3,6 @@ const util = require('util');
 const child_process = require('child_process');
 const rimraf = require('rimraf');
 const sign_util = require('app-builder-lib/electron-osx-sign/util');
-
 const asynfRimraf = util.promisify(rimraf);
 
 const DELETE_PATHS = [
@@ -13,24 +12,22 @@ const DELETE_PATHS = [
 const signCommand = path.join(__dirname, 'sign.sh');
 const entitlements = path.resolve(__dirname, '..', 'entitlements.plist');
 
-const signFile = (file, cwd) => {
+const signFile = file => {
     console.log(`Signing ${file}...`);
-
     child_process.execFileSync(signCommand, [
         file,
         entitlements
     ], {
-        cwd
+        cwd: path.dirname(file)
     });
 };
 
 exports.default = async function (context) {
-    const appName = `${context.packager.appInfo.productFilename}.app`;
-    const appPath = path.resolve(context.appOutDir, appName);
-
     // Remove anything we don't want in the final package
     for (const deletePath of DELETE_PATHS) {
-        await asynfRimraf(path.resolve(appPath, deletePath));
+        const resolvedPath = path.resolve(context.appOutDir, `${context.packager.appInfo.productFilename}.app`, deletePath);
+        console.log(`Deleting ${resolvedPath}...`);
+        await asynfRimraf(resolvedPath);
     }
 
     // Only continue for macOS
@@ -38,18 +35,17 @@ exports.default = async function (context) {
         return;
     }
 
-    let childPaths = await sign_util.walkAsync(appPath);
+    // Use app-builder-lib to find all binaries to sign, at this level it will include the final .app
+    let childPaths = await sign_util.walkAsync(context.appOutDir);
 
+    // Sign deepest first
     // From https://github.com/electron-userland/electron-builder/blob/master/packages/app-builder-lib/electron-osx-sign/sign.js#L120
     childPaths = childPaths.sort((a, b) => {
-        const aDepth = a.split(path.sep).length
-        const bDepth = b.split(path.sep).length
-        return bDepth - aDepth
+        const aDepth = a.split(path.sep).length;
+        const bDepth = b.split(path.sep).length;
+        return bDepth - aDepth;
     });
 
     // Sign binaries
     childPaths.forEach(file => signFile(file, context.appOutDir));
-
-    // Sign final app
-    signFile(appName, context.appOutDir);
 }
