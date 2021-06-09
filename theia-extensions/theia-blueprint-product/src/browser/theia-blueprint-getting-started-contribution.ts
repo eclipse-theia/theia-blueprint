64fed3ace1381cc837573ce10e48315dfee56747
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2021-06-09 10:29:26 +0200

    Getting started shown twice in help menu #110
    * avoid contributing the menu contributions twice
    
    Signed-off-by: Johannes Faltermeier <jfaltermeier@eclipsesource.com>

--- theia-extensions/theia-blueprint-product/src/browser/theia-blueprint-getting-started-contribution.ts
+++ theia-extensions/theia-blueprint-product/src/browser/theia-blueprint-getting-started-contribution.ts
@@ -14,24 +14,27 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
+import { AbstractViewContribution, FrontendApplication, FrontendApplicationContribution } from '@theia/core/lib/browser';
 import { inject, injectable } from 'inversify';
 
-import { FrontendApplication } from '@theia/core/lib/browser';
 import { FrontendApplicationStateService } from '@theia/core/lib/browser/frontend-application-state';
-import { GettingStartedContribution } from '@theia/getting-started/lib/browser/getting-started-contribution';
-import { WorkspaceService } from '@theia/workspace/lib/browser';
+import { GettingStartedWidget } from '@theia/getting-started/lib/browser/getting-started-widget';
+import { TheiaBlueprintGettingStartedWidget } from './theia-blueprint-getting-started-widget';
 
 @injectable()
-export class TheiaBlueprintGettingStartedContribution extends GettingStartedContribution {
+export class TheiaBlueprintGettingStartedContribution extends AbstractViewContribution<TheiaBlueprintGettingStartedWidget> implements FrontendApplicationContribution {
 
     @inject(FrontendApplicationStateService)
     protected readonly stateService: FrontendApplicationStateService;
 
-    @inject(WorkspaceService)
-    protected readonly workspaceService: WorkspaceService;
-
     constructor() {
-        super();
+        super({
+            widgetId: GettingStartedWidget.ID,
+            widgetName: GettingStartedWidget.LABEL,
+            defaultWidgetOptions: {
+                area: 'main',
+            }
+        });
     }
 
     async onStart(app: FrontendApplication): Promise<void> {
