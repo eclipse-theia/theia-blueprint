ed0a4d155bd94aeebee87b169135b692913e31bf
vince-fugnitto <vincent.fugnitto@ericsson.com>
2021-10-01 12:37:09 -0400

    electron: add example on updating supported api
    The commit adds an example in `theia-electron-main` (entrypoint) on how
    to override the supported vscode api version as an environment variable
    if needed.
    
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- applications/electron/scripts/theia-electron-main.js
+++ applications/electron/scripts/theia-electron-main.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const os = require('os')
 
+// Update to override the supported VS Code API version.
+// process.env.VSCODE_API_VERSION = '1.50.0'
+
 // Use a set of builtin plugins in our application.
 process.env.THEIA_DEFAULT_PLUGINS = `local-dir:${path.resolve(__dirname, '..', 'plugins')}`
 
