12d92d3ac3286d00a79b642fbbb87dfc09ab63e1
Alexandra Buzila <abuzila@eclipsesource.com>
2022-11-14 10:11:43 +0100

    Open current folder in Theia from terminal
    For AppImage applications, offer the option to create a script that
    allows to start blueprint from the command line by calling the 'theia'
    command.
    
    Signed-off-by: Alexandra Buzila <abuzila@eclipsesource.com>

---
+++ theia-extensions/theia-blueprint-launcher/src/browser/launcher-service.ts
@@ -0,0 +1,43 @@
+/********************************************************************************
+ * Copyright (C) 2022 EclipseSource and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+import { Endpoint } from '@theia/core/lib/browser';
+import { injectable } from '@theia/core/shared/inversify';
+
+@injectable()
+export class LauncherService {
+
+    async isInitialized(): Promise<boolean> {
+        const response = await fetch(new Request(`${this.endpoint()}/initialized`), {
+            body: undefined,
+            method: 'GET'
+        }).then(r => r.json());
+        return !!response?.initialized;
+    }
+
+    async createLauncher(create: boolean): Promise<void> {
+        fetch(new Request(`${this.endpoint()}`), {
+            body: JSON.stringify({ create }),
+            method: 'PUT',
+            headers: new Headers({ 'Content-Type': 'application/json' })
+        });
+    }
+
+    protected endpoint() {
+        const url = new Endpoint({ path: 'launcher' }).getRestUrl().toString();
+        return url.endsWith('/') ? url.slice(0, -1) : url;
+    }
+}
