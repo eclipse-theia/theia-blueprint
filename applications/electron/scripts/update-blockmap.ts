ede1c7656cfa07d302edcc6bcf980e2a6ae47845
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2023-04-03 10:59:04 +0200

    Update to 1.36.0
    * also upgrade dependencies
    * use vscode.git instead of @theia/git again
    
    Contributed on behalf of STMicroelectronics
    
    Signed-off-by: Johannes Faltermeier <jfaltermeier@eclipsesource.com>

--- applications/electron/scripts/update-blockmap.ts
+++ applications/electron/scripts/update-blockmap.ts
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (C) 2021 EclipseSource and others.
+ * Copyright (C) 2023 EclipseSource and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
