66a403276a1866f0196ee130b6f697e85f085e59
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2023-08-31 10:27:44 +0200

    No app icon for AppImage on Linux #296
    * consume image from applications/electron/resources/icons/WindowIcon/
    
    Contributed on behalf of STMicroelectronics
    
    Signed-off-by: Johannes Faltermeier <jfaltermeier@eclipsesource.com>

--- theia-extensions/product/src/electron-main/icon-contribution.ts
+++ theia-extensions/product/src/electron-main/icon-contribution.ts
@@ -14,10 +14,12 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import { ElectronMainApplication, ElectronMainApplicationContribution } from '@theia/core/lib/electron-main/electron-main-application';
-import { injectable } from '@theia/core/shared/inversify';
-import * as path from 'path';
 import * as os from 'os';
+import * as path from 'path';
+
+import { ElectronMainApplication, ElectronMainApplicationContribution } from '@theia/core/lib/electron-main/electron-main-application';
+
+import { injectable } from '@theia/core/shared/inversify';
 
 @injectable()
 export class IconContribution implements ElectronMainApplicationContribution {
@@ -28,7 +30,7 @@ export class IconContribution implements ElectronMainApplicationContribution {
             if (windowOptions && windowOptions.icon === undefined) {
                 // The window image is undefined. If the executable has an image set, this is used as a fallback.
                 // Since AppImage does not support this anymore via electron-builder, set an image for the linux platform.
-                windowOptions.icon = path.join(__dirname, '../../icons/512-512.png');
+                windowOptions.icon = path.join(__dirname, '../../resources/icons/WindowIcon/512-512.png');
             }
         }
     }
