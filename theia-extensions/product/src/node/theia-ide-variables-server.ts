b3f0e0db97469b0ad67f9aa2260a3d1001c273bb
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2024-01-26 06:40:20 +0100

    Theia 1.46.0 Update
    * update File URI imports
    * update built ins to 1.83.1
    * fix promote script (scp, since file are available in preview area
    already and there were issues with host identifiers)
    * update default values in promote script. these should work for the
    current release
    
    Contributed on behalf of STMicroelectronics

--- theia-extensions/product/src/node/theia-ide-variables-server.ts
+++ theia-extensions/product/src/node/theia-ide-variables-server.ts
@@ -10,7 +10,7 @@
 import * as os from 'os';
 import * as path from 'path';
 import { injectable } from '@theia/core/shared/inversify';
-import { FileUri } from '@theia/core/lib/node/file-uri';
+import { FileUri } from '@theia/core/lib/common/file-uri';
 import { EnvVariablesServerImpl } from '@theia/core/lib/node/env-variables';
 
 @injectable()
