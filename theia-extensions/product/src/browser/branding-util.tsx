c1f6b56c16f4bf674d60756ad5e6078c2b47f0a8
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2024-08-30 06:47:53 +0200

    Add collaboration getting started header #383
    * add missing space

--- theia-extensions/product/src/browser/branding-util.tsx
+++ theia-extensions/product/src/browser/branding-util.tsx
@@ -130,8 +130,8 @@ export function renderCollaboration(windowService: WindowService): React.ReactNo
         <div >
             The IDE features a built-in collaboration feature.
             You can share your workspace with others and work together in real-time by clicking on the <i>Collaborate</i> item in the status bar.
-            The collaboration feature is powered by the
-            <BrowserLink text="Open Collaboration Tools" url="https://www.open-collab.tools/" windowService={windowService} /> project
+            The collaboration feature is powered by
+            the <BrowserLink text="Open Collaboration Tools" url="https://www.open-collab.tools/" windowService={windowService} /> project
             and uses their public server infrastructure.
         </div>
     </div>;
