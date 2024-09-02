f3e185c978e58193b5a2024e069699b29f49f3d9
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2024-09-02 12:19:55 +0200

    Do not run TerserPlugin in parallel
    * especially in production mode this may lead to OOM issues on
    smaller/shared build nodes
    
    Contributed on behalf of STMicroelectronics

--- applications/electron/webpack.config.js
+++ applications/electron/webpack.config.js
@@ -5,6 +5,7 @@
 // @ts-check
 const configs = require('./gen-webpack.config.js');
 const nodeConfig = require('./gen-webpack.node.config.js');
+const TerserPlugin = require('terser-webpack-plugin');
 
 /**
  * Expose bundled modules on window.theia.moduleName namespace, e.g.
@@ -14,6 +15,29 @@ configs[0].module.rules.push({
     test: /\.js$/,
     loader: require.resolve('@theia/application-manager/lib/expose-loader')
 }); */
+
+/** 
+ * Do no run TerserPlugin with parallel: true
+ * Each spawned node may take the full memory configured via NODE_OPTIONS / --max_old_space_size
+ * In total this may lead to OOM issues
+ */ 
+if (nodeConfig.config.optimization) {
+    nodeConfig.config.optimization.minimizer = [
+        new TerserPlugin({
+            parallel: false,
+            exclude: /^(lib|builtins)\//
+        })
+    ];
+}
+for (const config of configs) {
+    config.optimization = {
+        minimizer: [
+            new TerserPlugin({
+                parallel: false
+            })
+        ]
+    };
+}
 
 module.exports = [
     ...configs,
